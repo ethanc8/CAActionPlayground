ed133d5e6586ad82af7008d258415c12b47d6072
ethanc8 <echaroenpitaks@gmail.com>
2024-06-03 17:08:52 -0500

    Do something with MyAction

--- main.m
+++ main.m
@@ -2,10 +2,17 @@
 #import <QuartzCore/CAAction.h>
 
 @interface MyAction <CAAction> : NSObject
+- (void)runActionForKey:(NSString *)key object:(id)anObject arguments:(NSDictionary *)dict;
 @end
 @implementation MyAction
+- (void)runActionForKey:(NSString *)key object:(id)anObject arguments:(NSDictionary *)dict {
+    NSLog(@"Hello, world!");
+}
 @end
 
 int main() {
+    MyAction* myAction = [[MyAction alloc] init];
+    [myAction runActionForKey:@"helloworld" object:@"helloworld" arguments:@{@"hello": @"world"}];
+    RELEASE(myAction);
     return 0;
 }
