f7f31be69aa0f6284611e549953777f842dc2ced
ethanc8 <echaroenpitaks@gmail.com>
2024-06-03 17:24:07 -0500

    Add category implementing CAAction

--- main.m
+++ main.m
@@ -10,9 +10,26 @@
 }
 @end
 
+@interface FakeAction : NSObject
+@end
+@implementation FakeAction
+@end
+
+@interface FakeAction(RealAction) <CAAction>
+- (void)runActionForKey:(NSString *)key object:(id)anObject arguments:(NSDictionary *)dict;
+@end
+@implementation FakeAction(RealAction)
+- (void)runActionForKey:(NSString *)key object:(id)anObject arguments:(NSDictionary *)dict {
+    NSLog(@"Hello, world!");
+}
+@end
+
 int main() {
     MyAction* myAction = [[MyAction alloc] init];
     [myAction runActionForKey:@"helloworld" object:@"helloworld" arguments:@{@"hello": @"world"}];
+    FakeAction* fakeAction = [[FakeAction alloc] init];
+    [fakeAction runActionForKey:@"helloworld" object:@"helloworld" arguments:@{@"hello": @"world"}];
     RELEASE(myAction);
+    RELEASE(fakeAction);
     return 0;
 }
